e8dedac4b0e73eb763f286bb6f33f8be55180345
Nicholas Koskowski <Nick@businessonmarketst.com>
2014-01-17 10:13:03 -0800

    Added Dropdowns

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="container">
 	<div class="content-area">
-		<section class="blog-content">
+		<section class="blog-post">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<h2><?php the_title(); ?></h2>
 			<?php the_content(); ?>
